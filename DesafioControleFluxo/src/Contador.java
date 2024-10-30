7d2c960a5f0d14cd751377b5bf89e733b520ecd9
joaopcs80 <joaopcs80@gmail.com>
2024-10-30 13:51:20 -0300

    att

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,8 +1,7 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Contador {
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
 		int parametroUm = terminal.nextInt();
@@ -18,7 +17,7 @@ public class Contador {
 			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 		}		
 	}
-	
+
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
        if (parametroUm > parametroDois) {
